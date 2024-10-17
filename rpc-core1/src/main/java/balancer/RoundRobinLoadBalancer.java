57f0eefc22344be087a8ebd6250030922c61bd6d
zhaoys0320 <121914647+zhaoys0320@users.noreply.github.com>
2024-10-17 17:25:51 +0800

    轮询负载均衡器
    轮询负载均衡器

---
+++ rpc-core1/src/main/java/balancer/RoundRobinLoadBalancer.java
@@ -0,0 +1,26 @@
+package balancer;
+
+import model.ServiceMetaInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RoundRobinLoadBalancer implements LoadBalancer{
+
+    //原子操作
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
+    @Override
+    public ServiceMetaInfo select(Map<String, Object> requestParams, List<ServiceMetaInfo> serviceMetaInfoList) {
+        if (serviceMetaInfoList.isEmpty()) {
+            return null;
+        }
+        int size = serviceMetaInfoList.size();
+        //只有一个服务不需要轮询
+        if(size == 1){
+            return serviceMetaInfoList.get(0);
+        }
+        int index = currentIndex.getAndIncrement() % size;
+        return serviceMetaInfoList.get(index);
+    }
+}
